222e5d8f4a57e4b283bc5eae42caed7fdedff286
zaman <zamanehsani@gmail.com>
2022-05-27 00:55:11 +0400

    made the auto migrate opt on deploy

--- dsu/settings.py
+++ dsu/settings.py
@@ -80,26 +80,26 @@ WSGI_APPLICATION = 'dsu.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
-
 # DATABASES = {
 #     'default': {
-#             'ENGINE': os.environ.get('DB_ENGINE'),
-#             'NAME':  os.environ.get('DB_NAME'),
-#             'USER': os.environ.get('DB_USER'),
-#             'PASSWORD': os.environ.get('DB_PASSWORD'),
-#             'HOST': os.environ.get('DB_HOST'),
-#             'PORT': os.environ.get('DB_PORT'),
-#             'OPTIONS': {
-#             'init_command': 'SET sql_mode="STRICT_TRANS_TABLES"',    
-#             }
-#         }
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
 #     }
+# }
+
+DATABASES = {
+    'default': {
+            'ENGINE': os.environ.get('DB_ENGINE'),
+            'NAME':  os.environ.get('DB_NAME'),
+            'USER': os.environ.get('DB_USER'),
+            'PASSWORD': os.environ.get('DB_PASSWORD'),
+            'HOST': os.environ.get('DB_HOST'),
+            'PORT': os.environ.get('DB_PORT'),
+            'OPTIONS': {
+            'init_command': 'SET sql_mode="STRICT_TRANS_TABLES"',    
+            }
+        }
+    }
 
 
 # Password validation
