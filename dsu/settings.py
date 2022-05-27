27f26e014fd3eadc02dc594c64dcbcf721e89044
zaman <zamanehsani@gmail.com>
2022-05-28 02:54:59 +0400

    one bit change in settings.py

--- dsu/settings.py
+++ dsu/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 import os
 from pathlib import Path
 import pymysql
+
 pymysql.version_info = (1, 4, 13, "final", 0)
 pymysql.install_as_MySQLdb()
 
