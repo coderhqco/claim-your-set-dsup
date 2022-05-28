151de869dd241f26a63a7bd838dc281ee9b56b7a
zaman <zamanehsani@gmail.com>
2022-05-28 17:50:32 +0400

    moved the SECRET KEY to env file and added the readme.md file

---
+++ README.md
@@ -0,0 +1,13 @@
+This is the Description of the DSU project. 
+Mr. Morgan Please update the description of this project. 
+
+how to set up the project on you local machine:
+1. make a directory anywhere in your machine
+2. inside the directory, create python virtualenv and activate it.
+3. clode this repo inside the directory
+4. install all the requirements inside the requirements.txt file
+    - for installing the packages; user `pip install > requirements.txt`
+5. setup the env file for setting config files
+    - email config
+    - database 
+        - DSU by default uses db.sqlite file database
--- dsu/settings.py
+++ dsu/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-psh2t@r(#p1@qb)&po8e=mn$0$i@97y)if3626^udnv87li-56'
+SECRET_KEY = os.environ.get('SECRET_KEY'),
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -81,26 +81,31 @@ WSGI_APPLICATION = 'dsu.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db.sqlite3',
-#     }
-# }
-
-DATABASES = {
-    'default': {
-            'ENGINE': os.environ.get('DB_ENGINE'),
-            'NAME':  os.environ.get('DB_NAME'),
-            'USER': os.environ.get('DB_USER'),
-            'PASSWORD': os.environ.get('DB_PASSWORD'),
-            'HOST': os.environ.get('DB_HOST'),
-            'PORT': os.environ.get('DB_PORT'),
-            'OPTIONS': {
-            'init_command': 'SET sql_mode="STRICT_TRANS_TABLES"',    
-            }
+import json
+if json.loads(os.environ.get('PRODUCTION').lower()):
+    # production 
+    print("running in production")
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': BASE_DIR / 'db.sqlite3',
         }
     }
+else:
+    print("runnig the app with main database")
+    DATABASES = {
+        'default': {
+                'ENGINE': os.environ.get('DB_ENGINE'),
+                'NAME':  os.environ.get('DB_NAME'),
+                'USER': os.environ.get('DB_USER'),
+                'PASSWORD': os.environ.get('DB_PASSWORD'),
+                'HOST': os.environ.get('DB_HOST'),
+                'PORT': os.environ.get('DB_PORT'),
+                'OPTIONS': {
+                'init_command': 'SET sql_mode="STRICT_TRANS_TABLES"',    
+                }
+            }
+        }
 
 
 # Password validation
