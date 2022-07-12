b8b044b53ad101794c41a407b995b0d1f62f0b8a
Zaman Ehsani <zamanehsani@gmail.com>
2022-07-12 18:32:55 +0400

    added the cors package and make some imporvement for api registeration

--- dsu/settings.py
+++ dsu/settings.py
@@ -42,7 +42,8 @@ INSTALLED_APPS = [
     'api.apps.ApiConfig',
     'vote.apps.VoteConfig',
     'rest_framework',
-    'rest_framework_simplejwt'
+    'rest_framework_simplejwt',
+    'corsheaders'
 
 ]
 
@@ -55,6 +56,7 @@ MIDDLEWARE = [
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
     "whitenoise.middleware.WhiteNoiseMiddleware",
+    'corsheaders.middleware.CorsMiddleware',
 ]
 
 ROOT_URLCONF = 'dsu.urls'
@@ -185,3 +187,12 @@ SIMPLE_JWT = {
 # this is for  the backend jwt auth. 
 JWT_SECRET_KEY = os.environ.get('JWT_SECRET_KEY')
 
+CORS_ALLOW_ALL_ORIGINS = True # If this is used then `CORS_ALLOWED_ORIGINS` will not have any effect
+CORS_ALLOW_CREDENTIALS = True
+# CORS_ALLOWED_ORIGINS = [
+#     'http://localhost:3030',
+# ] 
+# # If this is used, then not need to use `CORS_ALLOW_ALL_ORIGINS = True`
+# CORS_ALLOWED_ORIGIN_REGEXES = [
+#     'http://localhost:3030',
+# ]
