126f39504a961ab0838ee643231379e4d7d675b4
Zaman Ehsani <zamanehsani@gmail.com>
2023-11-09 00:30:16 +0400

    removed dtally and ntally fields from Bill Model. Also, removed the staticmethods from Bill models. finally, renamed vote_by to vote_by_fDel in BillVote Model and updated the params of vote_date and last_updated fields.

--- bills/models.py
+++ bills/models.py
@@ -1,43 +1,9 @@
 from django.db import models
-from django.db.models import F
-from django.db.models.functions import Cast
-from django.db.models.expressions import Func
-from model_utils import FieldTracker
 from django.contrib.auth.models import User
 # Create your models here.
 
-class JSONIncrement(Func):
-    function = "jsonb_set"
-
-    def __init__(self, full_path, value=1, **extra):
-        field_name, *key_path_parts = full_path.split("__")
-
-        if not field_name:
-            raise ValueError("`full_path` can not be blank.")
-
-        if len(key_path_parts) < 1:
-            raise ValueError("`full_path` must contain at least one key.")
-
-        key_path = ",".join(key_path_parts)
-
-        new_value_expr = Cast(
-            Cast(F(full_path), IntegerField()) + value,
-            CharField()
-        )
-
-        expressions = [
-            F(field_name),
-            Value(f"{{{key_path}}}"),
-            Cast(new_value_expr, JSONField())
-        ]
-
-        super().__init__(*expressions, output_field=JSONField(), **extra)
-
 
 class Bill(models.Model):
-
-    dtally = models.JSONField(blank=True, default=dict)
-    ntally = models.JSONField(blank=True, default=dict)
     created_at = models.DateTimeField(auto_now_add=True, editable=False)
     updated_at = models.DateTimeField(auto_now=True, editable=False)
     congress = models.IntegerField()
@@ -57,30 +23,9 @@ class Bill(models.Model):
     text = models.TextField()
     advice = models.TextField()
 
-    @staticmethod
-    # @database_sync_to_async  
-    def update_district_tally(bill_number,vote_c_before,vote_c_after,district_code):
-        path_after = f"dtally__{district_code}__{vote_c_after}"
-        path_before = f"dtally__{district_code}__{vote_c_before}"
-        Bill.objects.filter(number=bill_number).update(
-            JSONIncrement(path_after, value=1),
-            JSONIncrement(path_before, value=-1)
-        )
-
-    @staticmethod
-    # @database_sync_to_async
-    def update_national_tally(bill_number,vote_c_before,vote_c_after):
-        path_before = f"ntally__{vote_c_before}"
-        path_after = f"ntally__{vote_c_after}"
-        Bill.objects.filter(number=bill_number).update(
-            JSONIncrement(path_after, value=1),
-            JSONIncrement(path_before, value=-1)
-        )
-
     class Meta:
         ordering = ('-created_at',)
-
-
+    
 
 class BillVote(models.Model):
 
@@ -93,15 +38,10 @@ class BillVote(models.Model):
 
     bill = models.ForeignKey(Bill, on_delete=models.CASCADE)
     voter = models.ForeignKey(User, on_delete=models.CASCADE)
-    voted_by = models.BooleanField(default=False)
+
+    voted_by_fDel = models.BooleanField(default=False)
     your_vote = models.CharField(max_length=2, choices=VOTE_CHOICES, default='Px')
-    vote_date = models.DateTimeField()
-    last_update = models.DateTimeField()
+    vote_date = models.DateTimeField(auto_now_add=True)
+    last_update = models.DateTimeField(auto_now=True)
 
-    update_tracker = FieldTracker(fields=['your_vote'])
-
-    def save(self, *args, **kwargs):
-        if self.update_tracker.changed():
-            self.last_update = timezone.now()
-        super().save(*args, **kwargs)
 
