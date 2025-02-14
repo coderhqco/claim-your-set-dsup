b4006f6dd0d1723876d91359cd37565ee60355cc
Kyrie21323 <kp2733@nyu.edu>
2025-02-15 00:39:43 +0400

    created views testset for secdec under api folder

--- api/tests.py
+++ api/tests.py
@@ -3,6 +3,12 @@ from django.test import TestCase, Client
 from .models import TestingModel
 import datetime
 from django.utils import timezone
+from rest_framework.test import APITestCase, APIClient
+from rest_framework import status
+from django.contrib.auth.models import User
+from vote.models import Group
+from vote.models_secdel import SecDel
+
 
 class TestingModelModelTest(TestCase):
     def setUp(self):
@@ -28,3 +34,50 @@ class TestingModelModelTest(TestCase):
         self.assertEqual(self.t.is_test, True)
         self.assertEqual(self.t.tagline, 'I am a singer')
         self.assertEqual(self.t.schedule_date, datetime.date(2019, 1, 1))
+
+
+#secDel API Tests
+class SecDelViewTestCase(APITestCase):
+    def setUp(self):
+        #create a user and authenticate
+        self.user = User.objects.create_user(username="testuser", password="testpassword")
+        self.client = APIClient()
+        self.client.login(username="testuser", password="testpassword")
+
+        #create a group
+        self.group = Group.objects.create(
+            code="1234",
+            district=None,                                          #add a district if necessary
+            invitation_code="ABCD1234",
+            group_type=1,
+        )
+
+        #create a SecDel instance
+        self.secdel = SecDel.objects.create(
+            circle=self.group,
+            user=self.user
+        )
+
+    def test_create_secdel(self):
+        data = {
+            "circle": self.group.id,
+            "user": self.user.id
+        }
+        response = self.client.post("/api/sec-del/", data)
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+        self.assertEqual(SecDel.objects.count(), 2)                 #one already exists in setUp
+
+    def test_list_secdels(self):
+        response = self.client.get("/api/sec-del/")
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(len(response.data), 1)                     #only 1 record in setUp
+
+    def test_retrieve_secdel(self):
+        response = self.client.get(f"/api/sec-del/{self.secdel.id}/")
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(response.data["id"], self.secdel.id)
+
+    def test_unauthorized_access(self):
+        self.client.logout()
+        response = self.client.get("/api/sec-del/")
+        self.assertEqual(response.status_code, status.HTTP_401_UNAUTHORIZED)
